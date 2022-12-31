cf91c7004f0079b76bbb5dc8ac66a852b8e45636
Jad Nohra <jadnohra@gmail.com>
2022-12-31 14:47:46 +0100

    .

--- waymo-motion/data/thumb/test.md
+++ waymo-motion/data/thumb/test.md
@@ -1,1 +1,4 @@
-![](./uncompressed_scenario_training_training.tfrecord-00022-of-01000.gif)
+| [![](../thumb/uncompressed_scenario_training_training.tfrecord-00022-of-01000.gif)](../vid/uncompressed_scenario_training_training.tfrecord-00022-of-01000.gif)  | [![](../thumb/uncompressed_scenario_training_training.tfrecord-00021-of-01000.gif)](../vid/uncompressed_scenario_training_training.tfrecord-00021-of-01000.gif)  | [![](../thumb/uncompressed_scenario_training_training.tfrecord-00020-of-01000.gif)](../vid/uncompressed_scenario_training_training.tfrecord-00020-of-01000.gif)  |
+|---|---|---|
+|   |   |   |
+|   |   |   |
--- waymo-motion/regen-gallery-script.py
+++ waymo-motion/regen-gallery-script.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import itertools
+import html
 from infra.infra_locals import get_repo_path
 
 def get_data_dirpath():
@@ -12,6 +14,12 @@ def get_data_thumb_dirpath():
     return os.path.join(get_data_dirpath(), "thumb")
 
 def run_main():
+    def make_thumb_code(filename):
+        if filename:
+            thumb_filename = '../thumb/' + html.escape(filename)
+            vid_filename = '../vid/' + html.escape(filename)
+            return f'[![]({thumb_filename})]({vid_filename})'
+        return ''
     all_vid_files = []
     for filename in glob.iglob(get_data_vid_dirpath() + '/**/*', recursive=True):
         if filename.endswith('.gif'):
@@ -22,7 +30,17 @@ def run_main():
             all_thumb_files.append(os.path.abspath(filename)[len(get_data_thumb_dirpath())+1:])
     all_gallery_files = list(set(all_vid_files) & set(all_thumb_files))
 
-    print(all_gallery_files)
+    group_size = 9
+    gallery_groups = list(itertools.zip_longest(*(iter(all_gallery_files),) * group_size))
+
+    for group in gallery_groups:
+        md_code = f'''
+| {make_thumb_code(group[0])}  | {make_thumb_code(group[1])}  | {make_thumb_code(group[2])}  |
+|---|---|---|
+| {make_thumb_code(group[3])}  | {make_thumb_code(group[4])}  | {make_thumb_code(group[5])}  |
+| {make_thumb_code(group[6])}  | {make_thumb_code(group[7])}  | {make_thumb_code(group[8])}  |
+'''
+        print(md_code)
 
 if __name__ == "__main__":
     run_main()
