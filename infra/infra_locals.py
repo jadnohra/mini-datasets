120cec27fc2b3df665348915d99b6c1c384fbac4
Alexander Hans <ahans@users.noreply.github.com>
2023-01-02 15:36:31 +0100

    Get repo root at runtime
    This avoids the manual step of creating a file containing the repo root.
    We can get that by resolving the symlinks that Bazel puts into the
    sandbox.

--- infra/infra_locals.py
+++ infra/infra_locals.py
@@ -1,11 +1,5 @@
-import os
-import json
+from pathlib import Path
 
-def _get_this_script_path():
-    return os.path.split(os.path.realpath(__file__))[0]
-
-def _get_infra_locals_path():
-    return os.path.join(_get_this_script_path(), "infra_locals.json")
 
 def get_repo_path():
-    return json.load(open(_get_infra_locals_path()))["repo_path"]
+    return Path(__file__).resolve().parent.parent
